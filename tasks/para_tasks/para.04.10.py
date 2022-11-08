9ff15439d7b96eb388949e653005f33f1e99417f
victoryke <xp02vika@yandex.ru>
2022-11-08 08:37:32 +0300

    Para 04.10 corrected

--- tasks/para_tasks/para.04.10.py
+++ tasks/para_tasks/para.04.10.py
@@ -1,43 +1,44 @@
 numbers = {}
-def check_number(number):
-        if (number[0] == '+') and (number[1] == '7') and (len(number) == 12):
-            return number
-        elif (number[0] == "8") and (len(number) == 11):
-            checked_number = "+7" + number[1:]
-            return checked_number
-        elif (len(number) == 10):
-            checked_number = "+7" + number
-            return checked_number
-        else:
-            return False
-print("You can add, change, delete, end, sort, show")
+def get_num():
+    number=str(input())
+    if (number[0] == '+') and (number[1] == '7') and (len(number) == 12):
+        return number
+    elif (number[0] == "8") and (len(number) == 11):
+        ch_number = "+7" + number[1:]
+        return ch_number
+    elif (len(number) == 10):
+        ch_number = "+7" + number
+        return ch_number
+    else:
+        return False
+def get_name():
+    name = str(input("введите имя контакта"))
+    name=name.title().strip()
+    return name
+def get_full_number():
+    name = get_name()
+    number = get_num()
+    if (number):
+        numbers[name] = number
+    else:
+        print("неправильный номер")
+def change_num():
+    name = get_name()
+    number = get_num()
+    if name in numbers.keys()
+        numbers[name]=number
+    else:
+        print("неправильный номер")
+def delete_num():
+    name = get_name()
+    if (name in numbers.keys()):
+        del numbers[name]
+    else:
+        print("не существует имени")
 while True:
-    move = str(input('what type of move you wanna choose?'))
-    if (move == "add"):
-        name = str(input("Type name")).title()
-        number = str(input("Type_number"))
-        if check_number(number):
-            number = check_number(number)
-            numbers[name] = number
-        else:
-            print("WRONG NUMERO")
-
-    elif (move == 'change'):
-        name = str(input("Type name")).title()
-        number = str(input("Type_number"))
-        if check_number(number):
-            number = check_number(number)
-            numbers[name] = number
-        else:
-            print("WRONG NUMERO")
-    elif (move == "delete"):
-        name = str(input()).title()
-        if (name in numbers.keys()):
-            del numbers[name]
-    elif (move == "show"):
-        print(numbers)
-    elif (move == 'sort'):
-        numbers = sorted(numbers)
-    elif (move == "end"):
-        break
-print(numbers)
+    print("выбери действие: 0-добавить,1-изменить, 3-удалить, 2-показать")
+    step = str(input('Какое действие выберете'))
+    if(step == "0"):get_full_number()
+    elif(step == "1"):change_num()
+    elif (step == '2'):print(numbers)
+    elif (step == "3"):delete_num()
