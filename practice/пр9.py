af04d7d4d8b818c1bb4a53fb2090601b8e7ee050
victoryke <xp02vika@yandex.ru>
2022-12-13 12:04:07 +0300

    pr_9

---
+++ practice/пр9.py
@@ -0,0 +1,10 @@
+import os
+def showMenu(startPath):
+    print(f"Текущий каталог:{startPath}\n")
+    print ("Выберите действие")
+    print("0:сменить рабочий каталог")
+    print("1:преобразовать pdf в Docx")
+    print("2:преобразовать Doc/Docx в pdf")
+    print("3:Произвести сжатие изображений")
+    print("4:удалить группу файлов")
+    print("5:выход")
