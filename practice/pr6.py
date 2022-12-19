55b7c969a1316b2833fb85098d6fb5cf504833fb
victoryke <xp02vika@yandex.ru>
2022-12-19 19:30:43 +0300

    added error and words

--- practice/pr6.py
+++
@@ -1,7 +0,0 @@
-import re
-file = open("kutlak.txt", mode="r", encoding="UTF-8")
-text = file.readlines()
-pattern = r"(?:^Рейс) (\d+) (?:прибыл|отправился) (\w+) (\w+) (?:\w) (\d+:\d+:\d+)"
-for line in text:
-    print(re.match(pattern,line))
-
