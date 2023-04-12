4309e874bfe82cc70b442af5d500512a4c81e310
victoryke <xp02vika@yandex.ru>
2023-04-12 14:20:44 +0300

    11.py

---
+++ tasks/15/15.1.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    def rate_hw(self, student, course, grade):
+        student.grades[course] = [grade]
