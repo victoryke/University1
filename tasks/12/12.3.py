456e5796c361fea8b9edb9f9977853b38273e81d
zhamka1 <zhamilistono@gmail>
2023-05-23 23:23:12 +0300

    12.2

--- tasks/12/12.3.py
+++ tasks/12/12.3.py
@@ -1,15 +1,36 @@
 class Car:
+    color = ""
+    type = ""
+    year = ""
+    engineOn = False
     def __init__(self,color,type,year):
         self.color = color
         self.type = type
         self.year = year
-    def star(self):
-        print("Автомобиль заведен")
+    def start(self):
+        if self.engineOn:
+            print("Мотор уже работает")
+        else:
+            print("Автомобиль заведен")
+            engineOn = True
     def stop(self):
-        print("Автомобиль заглушен")
+        if self.engineOn:
+            print("Автомобиль заглушен")
+            engineOn = False
+        else:
+            print("Мотор не работает")
     def set_year(self,year):
         self.year = year
     def set_type(self,type):
         self.type = type
     def set_color(self,color):
         self.color = color
+nissan = Car("black", "sedan", "2007")
+nissan.start()
+nissan.start()
+nissan.stop()
+nissan.stop()
+nissan.set_year("2008")
+nissan.set_type("universal")
+nissan.set_color("white")
+print(nissan.color,nissan.type,nissan.year)
